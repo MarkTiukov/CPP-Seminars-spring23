304317559a0b23544108d49f9f2f5c77f1602c99
MarkTiukov <tyukov.mark@gmail.com>
2023-02-19 02:04:30 +0300

    exception introduction + logic eception

--- seminar13/part1/exceptions.cpp
+++ seminar13/part1/exceptions.cpp
@@ -1,7 +1,40 @@
-#pragma once
+/// exception basics
 
+#include <iostream>
+#include <vector>
 
+// class exception;
+
+void ThrowExceptionExample() {
+  auto e = std::exception();
+  throw e;
+}
+
+/// *Logic exceptions*
+/// It reports errors that are a consequence of
+/// faulty logic within the program such as
+/// violating logical preconditions or class invariants
+/// and may be preventable.
+void LogicException() {
+  // vector throws a length_error if resized above max_size
+  std::vector<int> vector;
+  vector.resize(vector.max_size()e + 1);
+}
 
 int main() {
+  try {
+    try {
+      ThrowExceptionExample();
+    } catch (const std::exception& e) {
+      std::cout << "what() returns: " << e.what() << std::endl;
+      throw; // rethrow
+    } catch (const std::exception& ex) {
+      std::cout << "consequential" << std::endl;
+    }
+  } catch (const std::exception& ex) {
+    std::cout << "non consequential" << std::endl;
+  }
+
+  LogicException();
 
 }
