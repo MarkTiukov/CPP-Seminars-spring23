777083925ad066b9cbddfcd0da78f09be746259a
MarkTiukov <tyukov.mark@gmail.com>
2023-02-25 19:19:35 +0300

    priority queue

--- seminar14/container_adapters/priority_queue.cpp
+++ seminar14/container_adapters/priority_queue.cpp
@@ -1,5 +1,7 @@
 #include <queue>
+
 #include <iostream>
+#include <string>
 
 /// https://en.cppreference.com/w/cpp/container/priority_queue
 
@@ -50,6 +52,72 @@ void minHeap() {
 
 }
 
+class Person {
+ public:
+
+  Person() = delete;
+  Person(std::string name) : name_(name) {}
+  Person(std::string name, size_t age) : name_(name), age_(age) {}
+
+  friend bool operator<(const Person& lhs, const Person& rhs) {
+    return lhs.age_ < rhs.age_;
+  }
+  friend bool operator>(const Person& lhs, const Person& rhs) {
+    return rhs < lhs;
+  }
+  friend bool operator<=(const Person& lhs, const Person& rhs) {
+    return !(rhs < lhs);
+  }
+  friend bool operator>=(const Person& lhs, const Person& rhs) {
+    return !(lhs < rhs);
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const Person& person) {
+    os << "\nname_: " << person.name_;
+    return os;
+  }
+
+ private:
+  std::string name_;
+  size_t age_ = 0;
+
+};
+
+void personHeap() {
+  std::vector<Person> data = {
+      Person("per0", 5),
+      Person("per1", 15),
+      Person("per2", 25),
+      Person("per3", 35),
+      Person("per4", 45),
+      Person("per5", 55),
+      Person("per6", 60),
+  };
+
+  std::priority_queue<Person> person_heap(data.begin(), data.end());
+
+  printHeap(person_heap);
+}
+
+struct CustomCompare {
+  bool operator()(const int& left, const int& right) {
+    if (left < 5 && right < 5) {
+      return left < right;
+    } else if (left > 5 && right > 5) {
+      return left > right;
+    } else {
+      return right < left;
+    }
+  }
+};
+
+void customCompareHeap() {
+  const auto data = {1, 2, 3, 4, 5, 6, 7, 8};
+  std::priority_queue<int, std::vector<int>, CustomCompare> custom_heap(data.begin(), data.end());
+
+  printHeap(custom_heap);
+}
+
 int main() {
-  minHeap();
+  personHeap();
 }
