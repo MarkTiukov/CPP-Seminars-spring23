ad8636c489681ad98c3437f5641b8c72d0325404
MarkTiukov <tyukov.mark@gmail.com>
2023-04-25 02:33:37 +0300

    lambda

--- seminar20/functions/function.cpp
+++ seminar20/functions/function.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
 #include <vector>
 
-int main() {
+struct S {
+  int b = 7;
 
+  void func(int a) {
+    std::cout << a + b << std::endl;
+  }
+
+};
+
+void member() {
+  void (S::* p)(int) = &S::func;
+  S s;
+  (s.*p)(5);
+
+  std::function<void(S&, int)> f = &S::func;
+  f(s, 7);
+}
+
+int foo(int x, int y) {
+  return x + y;
+}
+
+struct Callable {
+  int operator()(int x, int y) const {
+    return x / y;
+  }
+};
+
+int main() {
+  std::function<int(int, int)> f = foo;
+  std::cout << f(10, 10) << std::endl;
+
+  f = Callable();
+  std::cout << f(10, 10) << std::endl;
+
+  f = [](int x, int y) {return x * y; };
+  std::cout << f(10, 10) << std::endl;
 }
